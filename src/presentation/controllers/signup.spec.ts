189e82d7ad72dac00b534f54fffd304626cef53f
Elisandro <elisandro@ulbra.edu.br>
2020-03-13 20:55:00 -0300

    test: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from './signup'
+
+describe('Signup Controller', () => {
+  test('should 400 return if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
