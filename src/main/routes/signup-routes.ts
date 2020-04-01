8560849266ca40d5223710b074d770ac95ba6d17
Elisandro <elisandro@ulbra.edu.br>
2020-04-01 13:03:29 -0300

    feat: ensure logControllerDecorator calls controller with correct values

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
-import { makeSinnUpController } from '../factories/signup'
+import { makeSignUpController } from '../factories/signup'
 import { adapterRoute } from '../adapters/express-route-adapter'
 
 export default (router: Router): void => {
-  router.post('/signup', adapterRoute(makeSinnUpController()))
+  router.post('/signup', adapterRoute(makeSignUpController()))
 }
