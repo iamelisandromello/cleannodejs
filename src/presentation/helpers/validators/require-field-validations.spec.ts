d7a6b7ecda75e301b4982fe12dd64fcf51e5ac41
Elisandro <elisandro@ulbra.edu.br>
2020-04-04 17:52:06 -0300

    refactor: move sut creatiron to a factory method

--- src/presentation/helpers/validators/require-field-validations.spec.ts
+++ src/presentation/helpers/validators/require-field-validations.spec.ts
@@ -1,15 +1,19 @@
 import { RequireFieldValidation } from './require-field-validation'
 import { MissingParamError } from '../../errors'
 
+const makeSut = (): RequireFieldValidation => {
+  return new RequireFieldValidation('field')
+}
+
 describe('Require Field Validation', () => {
   test('Should return a MissingParamError fails', () => {
-    const sut = new RequireFieldValidation('field')
+    const sut = makeSut()
     const error = sut.validate({ name: 'any_name' })
     expect(error).toEqual(new MissingParamError('field'))
   })
 
   test('Should not return if validation fails successeds', () => {
-    const sut = new RequireFieldValidation('field')
+    const sut = makeSut()
     const error = sut.validate({ field: 'any_name' })
     expect(error).toBeFalsy()
   })
