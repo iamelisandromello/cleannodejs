939513c1d6da3f066c62aec18437380f54532e0e
Elisandro <elisandro@ulbra.edu.br>
2020-04-15 11:17:11 -0300

    refactor: separate factories in folders

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,4 +1,4 @@
-import { makeSignUpController } from '../factories/signup/signup-factory'
+import { makeSignUpController } from '../factories/controllers/signup/signup-controller-factory'
 import { adapterRoute } from '../adapters/express/express-route-adapter'
 import { Router } from 'express'
 
