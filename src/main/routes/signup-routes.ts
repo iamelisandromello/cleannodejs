e7d7d621d258c4ac9112d62b56918eb3fdece32b
Elisandro <elisandro@ulbra.edu.br>
2020-04-15 13:42:36 -0300

    feat: ensure DbAccount calls LoadAccountByEmailRepository with correct email

--- src/main/routes/signup-routes.ts
+++
@@ -1,7 +0,0 @@
-import { makeSignUpController } from '../factories/controllers/signup/signup-controller-factory'
-import { adapterRoute } from '../adapters/express/express-route-adapter'
-import { Router } from 'express'
-
-export default (router: Router): void => {
-  router.post('/signup', adapterRoute(makeSignUpController()))
-}
