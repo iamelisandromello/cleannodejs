8bf47923c9202c48f3bbb3e0f9afdfd58f891102
Elisandro <elisandro@ulbra.edu.br>
2020-04-18 13:55:11 -0300

    feat: ensure SurveyMongoRepository add a new survey on add success

--- src/data/usecases/add-survey/db-add-survey.ts
+++ src/data/usecases/add-survey/db-add-survey.ts
@@ -1,9 +1,9 @@
 import { AddSurvey, AddSurveyModel, AddSurveyRepository } from './db-add-survey-protocols'
 
 export class DbAddSurvey implements AddSurvey {
-  constructor (private readonly addSurveyREpository: AddSurveyRepository) {}
+  constructor (private readonly addSurveyRepository: AddSurveyRepository) {}
 
   async add (data: AddSurveyModel): Promise<void> {
-    await this.addSurveyREpository.add(data)
+    await this.addSurveyRepository.add(data)
   }
 }
