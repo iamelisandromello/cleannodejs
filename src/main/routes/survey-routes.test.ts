eb8cd974f0c4962ede68eb04c6b2fc3bc8fb3a1c
Elisandro <elisandro@ulbra.edu.br>
2020-04-21 17:43:07 -0300

    fix: ensure AccountMongoRepository allows admn to access any route

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -38,37 +38,37 @@ describe('Survey Routes', () => {
         })
         .expect(403)
     })
-  })
 
-  test('Should return 204 on add survey with accessToken', async () => {
-    const res = await accountCollection.insertOne({
-      name: 'Elisandro',
-      email: 'elisandromello@hotmail.com',
-      password: '123',
-      role: 'admin'
-    })
-    const id = res.ops[0]._id
-    const accessToken = sign({ id }, env.jwtSecret)
-    await accountCollection.updateOne({
-      _id: id
-    }, {
-      $set: {
-        accessToken
-      }
-    })
-
-    await request(app)
-      .post('/api/surveys')
-      .set('x-access-token', accessToken)
-      .send({
-        question: 'Question',
-        answers: [{
-          answer: 'Answer 1',
-          image: 'http://image-name.com'
-        }, {
-          answer: 'Answer 2'
-        }]
+    test('Should return 204 on add survey with accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'Elisandro',
+        email: 'elisandromello@hotmail.com',
+        password: '123',
+        role: 'admin'
       })
-      .expect(403)
+      const id = res.ops[0]._id
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Question',
+          answers: [{
+            answer: 'Answer 1',
+            image: 'http://image-name.com'
+          }, {
+            answer: 'Answer 2'
+          }]
+        })
+        .expect(403)
+    })
   })
 })
