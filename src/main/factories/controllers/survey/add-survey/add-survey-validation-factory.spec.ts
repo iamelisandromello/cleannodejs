f274e1e3789e10db68a1afb595063f401e541999
Elisandro <elisandro@ulbra.edu.br>
2020-05-01 01:00:18 -0300

    refactor: change all jest.mock paths to use modules

--- src/main/factories/controllers/survey/add-survey/add-survey-validation-factory.spec.ts
+++ src/main/factories/controllers/survey/add-survey/add-survey-validation-factory.spec.ts
@@ -2,7 +2,7 @@ import { makeAddSurveyValidation } from './add-survey-validation-factory'
 import { Validation } from '@/presentation/protocols/validation'
 import { ValidationComposite, RequireFieldValidation } from '@/validation/validators'
 
-jest.mock('../../../../../validation/validators/validation-composite')
+jest.mock('@/validation/validators/validation-composite')
 
 describe('AddSurveyValidation Factory', () => {
   test('Should call ValidationComposite with all validations', () => {
