a5d27e266f2e15c4070d21ed0207e9f8247ba5ac
Elisandro Mello <elisandro@ulbra.edu.br>
2020-05-08 11:51:41 -0300

    test: ensure SaveSurveyResultController returns 403 if an invalid answe is provided

--- src/presentation/controllers/survey-result/save-survey-reult/save-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/save-survey-reult/save-survey-result-controller.spec.ts
@@ -6,6 +6,9 @@ import { forbidden, serverError } from '@/presentation/helpers/http/http-helper'
 const makeFakeRequest = (): HttpRequest => ({
   params: {
     surveyId: 'any_survey_id'
+  },
+  body: {
+    answer: 'any_answer'
   }
 })
 
@@ -50,7 +53,7 @@ describe('SaveSurveyResult Controller', () => {
     expect(loadById).toHaveBeenCalledWith('any_survey_id')
   })
 
-  test('Should return if LoadSurveyById  returns null', async () => {
+  test('Should return 403 if LoadSurveyById  returns null', async () => {
     const { sut, loadSurverByIdStub } = makeSut()
     jest.spyOn(loadSurverByIdStub, 'loadById').mockResolvedValueOnce(new Promise(resolve => resolve(null)))
     const httpResponse = await sut.handle(makeFakeRequest())
@@ -62,5 +65,18 @@ describe('SaveSurveyResult Controller', () => {
     jest.spyOn(loadSurverByIdStub, 'loadById').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 403 if an invalid answer is provided', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle({
+      params: {
+        surveyId: 'any_survey_id'
+      },
+      body: {
+        answer: 'wrong_answer'
+      }
+    })
+    expect(httpResponse).toEqual(forbidden(new InvalidParamError('answer')))
   })
 })
