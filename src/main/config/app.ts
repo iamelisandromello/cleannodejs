b5841c8184d5d064f924fc24bd8907ed1a91f1cb
Elisandro Mello <elisandro@ulbra.edu.br>
2020-05-29 21:25:13 -0300

    feat: add swagger api

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,9 +1,11 @@
 import setupMiddlewares from './middlewares'
 import setupRoutes from './routes'
 import setupStaticFiles from './static-files'
+import setupSwagger from './config-swagger'
 import express from 'express'
 
 const app = express()
+setupSwagger(app)
 setupStaticFiles(app)
 setupMiddlewares(app)
 setupRoutes(app)
---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node TypeScript',
+    description: 'Api Curso de Clen Architecture em TypeScript',
+    version: '1.0.0'
+  }
+}
