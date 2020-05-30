f73568e2e6b056f9532b3877e268e937ae431249
Elisandro Mello <elisandro@ulbra.edu.br>
2020-05-29 23:38:11 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,25 @@
+import { loginPath } from './paths/login-paths'
+import { accountSchema } from './schemas/account-schema'
+import { loginParamSchema } from './schemas/login-params-schema'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node TypeScript',
     description: 'Api Curso de Clen Architecture em TypeScript',
     version: '1.0.0'
+  },
+  servers: [{
+    url: '/api'
+  }],
+  tags: [{
+    name: 'Login'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamSchema
   }
 }
